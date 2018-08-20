a57fbd8329e6c2260ef7aac7ffccd7785abd6810
Jacob Lewallen <jlewallen@gmail.com>
2018-08-20 12:00:12 -0700

    Set firmware compiled time.

--- firmware/module/main.cpp
+++ firmware/module/main.cpp
@@ -25,6 +25,7 @@ void setup() {
 
     firmware_version_set(FIRMWARE_GIT_HASH);
     firmware_build_set(FIRMWARE_BUILD);
+    firmware_compiled_set(DateTime(__DATE__, __TIME__).unixtime());
 
     loginfof("Module", "Starting (%lu free)", fk_free_memory());
 
