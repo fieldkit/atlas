44308fa4be77027beeb8b0490a6038db2a9601bb
Jacob Lewallen <jlewallen@gmail.com>
2018-10-31 11:02:37 -0700

    Set minimum on time for peripherals.
    This is to avoid accidentally erasing atlas module EEPROM memory by power
    cycling them too quickly.

--- firmware/module/atlas_hardware.h
+++ firmware/module/atlas_hardware.h
@@ -8,6 +8,12 @@ namespace fk {
 constexpr uint8_t FK_ATLAS_PIN_FLASH = 5;
 constexpr uint8_t FK_ATLAS_PIN_PERIPH_ENABLE = 12;
 
+/**
+ * Minimum time we can enable the peripherals for. This is to avoid Atlas module
+ * EEPROM corruption.
+ */
+constexpr uint32_t FK_ATLAS_PERIPH_MINIMUM_ENABLE_TIME = 1000;
+
 }
 
 #endif
--- firmware/module/atlas_module.cpp
+++ firmware/module/atlas_module.cpp
@@ -4,7 +4,7 @@
 namespace fk {
 
 AtlasModule::AtlasModule(ModuleInfo &info) :
-    Module(moduleBus, info, { FK_ATLAS_PIN_FLASH, FK_ATLAS_PIN_PERIPH_ENABLE }) {
+    Module(moduleBus, info, { FK_ATLAS_PIN_FLASH, FK_ATLAS_PIN_PERIPH_ENABLE, FK_ATLAS_PERIPH_MINIMUM_ENABLE_TIME }) {
 }
 
 void AtlasModule::begin() {
