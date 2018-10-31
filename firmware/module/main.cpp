5637e214567b9344f758d7f396cb7fb74a711273
Jacob Lewallen <jlewallen@gmail.com>
2018-10-31 13:19:09 -0700

    Open debug serial if USB is unavailable.

--- firmware/module/main.cpp
+++ firmware/module/main.cpp
@@ -17,6 +17,13 @@ void setup() {
         delay(100);
     }
 
+    if (!Serial) {
+        // The call to end here seems to free up some memory.
+        Serial.end();
+        Serial5.begin(115200);
+        log_uart_set(Serial5);
+    }
+
     firmware_version_set(FIRMWARE_GIT_HASH);
     firmware_build_set(FIRMWARE_BUILD);
     firmware_compiled_set(DateTime(__DATE__, __TIME__).unixtime());
